7a1c580482b95aa66a0c596a3cb1958461fd9e1b
Łukasz Staniszewski <2277lukas@gmail.com>
2020-02-10 00:38:05 +0100

    Create PetTypeServiceMap. Closes #7

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author Łukasz Staniszewski on 2020-02-10
+ * @project sfg-pet-clinic
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
