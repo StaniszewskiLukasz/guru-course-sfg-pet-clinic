7a731151790d44ecf252117a34c74a23a3e2f104
Łukasz Staniszewski <2277lukas@gmail.com>
2020-02-10 00:21:54 +0100

    Added Visit entity. Closes #

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author Łukasz Staniszewski on 2020-01-27
  * @project sfg-pet-clinic
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
