a3d218b5baaa69a83628b9e688aec3fb35351511
Łukasz Staniszewski <2277lukas@gmail.com>
2020-02-15 22:17:22 +0100

    Implemented Vet entity. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,12 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Łukasz Staniszewski on 2020-02-10
  * @project sfg-pet-clinic
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
+
+//    @ManyToMany(mappedBy = "vet")
+//    private Set<Vet> vets= new HashSet<>();
 
     public String getDescription() {
         return description;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,9 +8,15 @@ import java.util.Set;
  * @author Łukasz Staniszewski on 2020-01-27
  * @project sfg-pet-clinic
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-private Set<Speciality> specialties = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns =@JoinColumn(name = "speciality_id") )
+    private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
         return specialties;
