5aa290287a17e1697fc6e61990b8f9ecf8bd319e
Łukasz Staniszewski <2277lukas@gmail.com>
2020-02-19 16:47:14 +0100

    First Junit 5 test

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author Łukasz Staniszewski on 2020-02-19
+ * @project sfg-pet-clinic
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+    @Test
+    void contextLoad() {
+    }
+}
